4f47aa50c73f383cdd27b446d7dce44fc7ad703a
André Luan <ra90857@uem.br>
2018-09-24 23:41:17 -0300

    Adicionado atributos a pessoa

--- src/main/java/BiblioTec/Domain/Usuario.java
+++ src/main/java/BiblioTec/Domain/Usuario.java
@@ -9,58 +9,11 @@ import javax.persistence.*;
  */
 @Entity
 @Table(name = "Usuario")
-public class Usuario {
+public class Usuario extends Person{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    @Column(name = "Nome")
-    private String nome;
-
-    @Column(name = "Sobrenome")
-    private String sobrenome;
-
-    @Column(name = "Idade")
-    private int idade;
-
-    public Usuario() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public Usuario(String nome, String sobrenome, int idade) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.idade = idade;
-    }
-
+    private String situacao;
+    
+    private String detalhesInadimplencia;
     
     
 }
